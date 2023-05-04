f7c48c43c2cacdf5ff12eef5c1d7a392c0ceed25
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-05-04 10:13:07 +0900

    Update quotation_conditions.js
    견적상황 grid ref reloadData 시도

--- pages/quotation/quotation_conditions.js
+++ pages/quotation/quotation_conditions.js
@@ -1,8 +1,16 @@
 import BaseLayout from "../../components/base_layout";
 import EstimateNav from "../../components/navi/estimate_nav";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import dynamic from "next/dynamic";
 import style from "../../styles/Home.module.css";
+import $ from "jquery";
+
+const EstimateGrid = dynamic(
+  () => import("../../components/quotation/quotation_detail"),
+  {
+    ssr: false,
+  }
+);
 
 const EstimateConditionsGrid = dynamic(
   () => import("../../components/quotation/quotation_conditions_table"),
@@ -13,23 +21,50 @@ const EstimateConditionsGrid = dynamic(
 
 // 전역변수
 const axios = require("axios").default;
-export default function EstimateDetail() {
+export default function EstimateDetail(props) {
+  const ref = useRef();
+  const ref_grid = useRef();
+
+  const quotationGridShow = (props) => {
+    $(".quotationGrid").show();
+    $(".quotationConditions").hide();
+    // ref_grid.current.getInstance().setRequestParams(props.params);
+    console.log(ref_grid);
+    console.log(props);
+  };
+
+  const quotationConditionsShow = (props) => {
+    $(".quotationGrid").hide();
+    $(".quotationConditions").show();
+    ref.current.getInstance().readData();ㅋ
+    console.log(ref);
+  };
+
   return (
     <BaseLayout>
       <EstimateNav />
       <div className="flex-center" id="quotation_reply_grid">
         <div className={style.quotation_reply_menu}>
           <h4 className="">견적관리</h4>
-          <a href="/quotation/quotation_reply_list" className="">
+          <button className="" onClick={quotationGridShow}>
             견적회신하기
-          </a>
+          </button>
           <div>ㅣ</div>
-          <a href="/quotation/quotation_conditions" className="">
+          <button href="#" className="" onClick={quotationConditionsShow}>
             견적상황
-          </a>
+          </button>
         </div>
-
-        <EstimateConditionsGrid />
+        <div className="quotationGrid">
+          <EstimateGrid
+            ref={ref_grid}
+            onBeforeRequest={(props) => {
+              props.instance.setRequestParams(props.params);
+            }}
+          />
+        </div>
+        <div className="quotationConditions" style={{ display: "none" }}>
+          <EstimateConditionsGrid ref={ref} />
+        </div>
       </div>
     </BaseLayout>
   );
