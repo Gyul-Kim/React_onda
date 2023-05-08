37f3e306c1001efaee1c27bf331526ee1b0b62fa
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-05-08 17:39:23 +0900

    Update quotation_reply.js
    quotation reply 불필요 코드 변경

--- pages/quotation/quotation_reply.js
+++ pages/quotation/quotation_reply.js
@@ -1,7 +1,10 @@
 import BaseLayout from "../../components/base_layout";
 import EstimateNav from "../../components/navi/estimate_nav";
-import React, { useEffect, useState } from "react";
+import QuotationNav from "../../components/navi/quotation_nav";
+
+import React, { useEffect, useState, useRef } from "react";
 import dynamic from "next/dynamic";
+import Pagination from "react-js-pagination";
 import style from "../../styles/Home.module.css";
 
 const EstimateGrid = dynamic(
@@ -11,25 +14,22 @@ const EstimateGrid = dynamic(
   }
 );
 
-
 // 전역변수
-const axios = require("axios").default;
 export default function EstimateDetail() {
   return (
     <BaseLayout>
       <EstimateNav />
-      <div className="flex-center" id="quotation_reply_grid">
+      <div id="quotation_reply_list">
+        <QuotationNav />
+      </div>
+
+      <div className="" id="quotation_reply_grid">
         <div className={style.quotation_reply_menu}>
-          <h4 className="">견적관리</h4>
-          <a href="/quotation/quotation_reply_list" className="">
-            견적회신하기
-          </a>
-          <div>ㅣ</div>
-          <a href="/quotation/quotation_conditions" className="">
-            견적상황
-          </a>
+          <h4 className="">견적회신하기</h4>
         </div>
-        <EstimateGrid />
+        <div className="quotationGrid">
+          <EstimateGrid />
+        </div>
       </div>
     </BaseLayout>
   );
