21431b0fc34b9a7851586dc03fb51f371e627570
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-05-10 17:36:37 +0900

    Update filledlinechart.js
    filledlinechart_막대 그래프 수정

--- components/chart/filledlinechart.js
+++ components/chart/filledlinechart.js
@@ -46,27 +46,29 @@ function FilledLineChart() {
           },
         ],
       },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false,
+      },
     });
   }, []);
 
   return (
     <>
       {/* Bar chart */}
-      <h1 className="w-[150px] mx-auto mt-10 text-xl font-semibold capitalize ">
-        Bar Chart
-      </h1>
+
       <div className="w-[1100px] h-screen flex mx-auto my-auto">
         <div className="border border-gray-400 pt-0 rounded-xl  w-full h-fit my-auto  shadow-xl">
-          <canvas
-            id="FilledLineChart"
-            style={
-              {
-                //   position: "absolute",
-                //   width: "100% !important",
-                //   height: "100% !important",
-              }
-            }
-          ></canvas>
+          <div>
+            <canvas
+              id="FilledLineChart"
+              style={{
+                position: "absolute",
+                width: "100% !important",
+                height: "95% !important",
+              }}
+            ></canvas>
+          </div>
         </div>
       </div>
     </>
@@ -74,3 +76,4 @@ function FilledLineChart() {
 }
 
 export default FilledLineChart;
+
