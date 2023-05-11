19dbb2e762a72573d09bb10f08ef2e73fd1830b3
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-05-11 12:31:38 +0900

    Update index.js
    메인페이지 대시보드 위치 수정

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import styles from "../styles/Home.module.css";
 import { Link, Box } from "@chakra-ui/react";
 import IndexLayout from "../components/index_layout";
 import DoughnutChart from "../components/chart/doughnut";
@@ -15,33 +14,52 @@ export default function Home() {
   return (
     <IndexLayout>
       <div className={style.main_container}>
-        <div className={style.main_lft}>
-          <div className={style.main_tp}>
-            <div className={style.bx_indicator}>
-              <h4>견적요청 건수</h4>
-              <p>3,456</p>
-              <p>+ 2.3%</p>
-              <p>Since Yesterday</p>
-            </div>
-            <div className={style.bx_indicator}>
-              <h4>견적회신건수</h4>
-              <p>3,456</p>
-              <p>+ 2.3%</p>
-              <p>Since Yesterday</p>
-            </div>
-            <div className={style.bx_indicator}>
-              <h4>주문건수</h4>
-              <p>3,456</p>
-              <p>+ 2.3%</p>
-              <p>Since Yesterday</p>
-            </div>
+        <div className={style.main_tp}>
+          <div className={style.bx_indicator}>
+            <h4>견적요청 건수</h4>
+            <p>3,456</p>
+            <p>+ 2.3%</p>
+            <p>Since Yesterday</p>
           </div>
-          <div className={style.main_btm}>
-            <DoughnutChart />
+          <div className={style.bx_indicator}>
+            <h4>견적회신건수</h4>
+            <p>3,456</p>
+            <p>+ 2.3%</p>
+            <p>Since Yesterday</p>
+          </div>
+          <div className={style.bx_indicator}>
+            <h4>주문건수</h4>
+            <p>3,456</p>
+            <p>+ 2.3%</p>
+            <p>Since Yesterday</p>
+          </div>
+          <div className={style.bx_indicator}>
+            <h4>월 거래액</h4>
+            <p>3,456</p>
+            <p>+ 2.3%</p>
+            <p>Since Yesterday</p>
+          </div>
+          <div className={style.bx_indicator}>
+            <h4>연 거래액</h4>
+            <p>3,456</p>
+            <p>+ 2.3%</p>
+            <p>Since Yesterday</p>
           </div>
         </div>
-        <div className={style.main_rt}>
-          <FilledLineChart />
+
+        <div className={style.main_md}>
+          <div className={style.main_lft}>
+            <h3>거래액</h3>
+            <div>
+              <FilledLineChart />
+            </div>
+          </div>
+          <div className={style.main_rt}>
+            <h3>Hit Rate</h3>
+            <div>
+              <DoughnutChart />
+            </div>
+          </div>
         </div>
       </div>
     </IndexLayout>
