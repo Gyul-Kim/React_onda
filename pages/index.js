7bb01d8d1e567149fcd623e421f810b25bb2a29e
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-05-12 16:56:02 +0900

    Update index.js
    index_table 추가 및 box, chart 정리

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Link, Box } from "@chakra-ui/react";
 import IndexLayout from "../components/index_layout";
-import DoughnutChart from "../components/chart/doughnut";
-import FilledLineChart from "../components/chart/filledlinechart";
+import BoxIndicator from "../components/box/box";
+import ChartAll from "../components/chartAll";
 
 import React, { useState, useEffect } from "react";
 import { GetReviewData } from "../pages/api/base";
@@ -14,51 +14,63 @@ export default function Home() {
   return (
     <IndexLayout>
       <div className={style.main_container}>
-        <div className={style.main_tp}>
-          <div className={style.bx_indicator}>
-            <h4>견적요청 건수</h4>
-            <p>3,456</p>
-            <p>+ 2.3%</p>
-            <p>Since Yesterday</p>
-          </div>
-          <div className={style.bx_indicator}>
-            <h4>견적회신건수</h4>
-            <p>3,456</p>
-            <p>+ 2.3%</p>
-            <p>Since Yesterday</p>
-          </div>
-          <div className={style.bx_indicator}>
-            <h4>주문건수</h4>
-            <p>3,456</p>
-            <p>+ 2.3%</p>
-            <p>Since Yesterday</p>
-          </div>
-          <div className={style.bx_indicator}>
-            <h4>월 거래액</h4>
-            <p>3,456</p>
-            <p>+ 2.3%</p>
-            <p>Since Yesterday</p>
-          </div>
-          <div className={style.bx_indicator}>
-            <h4>연 거래액</h4>
-            <p>3,456</p>
-            <p>+ 2.3%</p>
-            <p>Since Yesterday</p>
-          </div>
-        </div>
+        <BoxIndicator />
+        {/* <ChartAll /> */}
 
-        <div className={style.main_md}>
-          <div className={style.main_lft}>
-            <h3>거래액</h3>
-            <div>
-              <FilledLineChart />
-            </div>
+        <div className={style.main_btm}>
+          <div className={style.btm_title}>
+            <h3>회원목록</h3>
+            <button>+ More</button>
           </div>
-          <div className={style.main_rt}>
-            <h3>Hit Rate</h3>
-            <div>
-              <DoughnutChart />
-            </div>
+          <div className={style.tableWrap}>
+            <table>
+              <thead>
+                <tr>
+                  <th>순번</th>
+                  <th>회원정보</th>
+                  <th>주문건수</th>
+                  <th>가입날짜</th>
+                  <th>회원레벨</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>5</td>
+                  <td>bright</td>
+                  <td>12</td>
+                  <td>23-04-19</td>
+                  <td>일반</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>dfrot</td>
+                  <td>33</td>
+                  <td>23-04-09</td>
+                  <td>일반</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>arrow</td>
+                  <td>62</td>
+                  <td>23-04-04</td>
+                  <td>일반</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>mouser</td>
+                  <td>53</td>
+                  <td>23-04-01</td>
+                  <td>일반</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>digikey</td>
+                  <td>45</td>
+                  <td>23-03-15</td>
+                  <td>일반</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
