b1c013eecd78b57d270f0f67089968b75ede4b4d
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-05-16 17:29:16 +0900

    Update chartAll.js
    lineChart 월별 연별 버튼 추가

--- components/chartAll.js
+++ components/chartAll.js
@@ -6,14 +6,20 @@ export default function ChartAll() {
   return (
     <div className={style.main_md}>
       <div className={style.main_lft}>
-        <h3>거래액</h3>
-        <div>
+        <div style={{ display: "flex", width: "92%" }}>
+          <h3>거래액</h3>
+          <button>월별</button>
+          <button>연별</button>
+        </div>
+
+        <div className={style.chart_frame}>
           <FilledLineChart />
         </div>
       </div>
       <div className={style.main_rt}>
         <h3>Hit Rate</h3>
-        <div>
+
+        <div className={style.chart_frame}>
           <DoughnutChart />
         </div>
       </div>
