74043ab21b2869bb1015c2c50c557fb1c1bc4580
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-05-17 17:45:23 +0900

    Update filledlinechart.js
    line chart 월별 및 연별 그래프로 분할

--- components/chart/filledlinechart.js
+++ components/chart/filledlinechart.js
@@ -4,43 +4,106 @@ import style from "../../styles/Home.module.css";
 
 function FilledLineChart() {
   useEffect(() => {
-    var ctx = document.getElementById("FilledLineChart").getContext("2d");
+    var ctx = document.getElementById("monthLineChart").getContext("2d");
+    var ctx2 = document.getElementById("annualLineChart").getContext("2d");
     var myChart = new Chart(ctx, {
       type: "line",
       data: {
         labels: [
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
+          "1월",
+          "2월",
+          "3월",
+          "4월",
+          "5월",
+          "6월",
+          "7월",
+          "8월",
+          "9월",
+          "10월",
+          "11월",
+          "12월",
         ],
         datasets: [
           {
-            data: [86, 114, 106, 106, 107, 111, 133],
+            data: [86, 114, 106, 106, 107, 111, 133, 145, 122, 155, 180, 165],
+            label: "Hit",
+            borderColor: "#3e95cd",
+            backgroundColor: "#7bb6dd",
+            fill: false,
+          },
+          {
+            data: [70, 90, 44, 60, 83, 90, 100, 80, 70, 60, 120, 130, 109, 108],
+            label: "Progress",
+            borderColor: "#3cba9f",
+            backgroundColor: "#71d1bd",
+            fill: false,
+          },
+          // {
+          //   data: [10, 21, 60, 44, 17, 21, 17],
+          //   label: "Pending",
+          //   borderColor: "#ffa500",
+          //   backgroundColor: "#ffc04d",
+          //   fill: false,
+          // },
+          {
+            data: [6, 3, 2, 2, 7, 0, 16, 40, 50, 60, 30, 20, 10],
+            label: "Miss",
+            borderColor: "#c45850",
+            backgroundColor: "#d78f89",
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+
+        scales: {
+          xAxes: [
+            {
+              display: true,
+            },
+          ],
+          yAxes: [
+            {
+              display: true,
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    var myChart2 = new Chart(ctx2, {
+      type: "line",
+      data: {
+        labels: ["2020", "2021", "2022", "2023", "2024"],
+        datasets: [
+          {
+            data: [86, 114, 106, 106, 107],
             label: "Applied",
             borderColor: "#3e95cd",
             backgroundColor: "#7bb6dd",
             fill: false,
           },
           {
-            data: [70, 90, 44, 60, 83, 90, 100],
+            data: [70, 90, 44, 60, 83],
             label: "Accepted",
             borderColor: "#3cba9f",
             backgroundColor: "#71d1bd",
             fill: false,
           },
+          // {
+          //   data: [10, 21, 60, 44, 17, 21, 17],
+          //   label: "Pending",
+          //   borderColor: "#ffa500",
+          //   backgroundColor: "#ffc04d",
+          //   fill: false,
+          // },
           {
-            data: [10, 21, 60, 44, 17, 21, 17],
-            label: "Pending",
-            borderColor: "#ffa500",
-            backgroundColor: "#ffc04d",
-            fill: false,
-          },
-          {
-            data: [6, 3, 2, 2, 7, 0, 16],
+            data: [6, 3, 2, 2, 7],
             label: "Rejected",
             borderColor: "#c45850",
             backgroundColor: "#d78f89",
@@ -51,6 +114,7 @@ function FilledLineChart() {
       options: {
         responsive: true,
         maintainAspectRatio: false,
+
         scales: {
           xAxes: [
             {
@@ -75,7 +139,8 @@ function FilledLineChart() {
       {/* Bar chart */}
 
       <div className={style.earning_chart}>
-        <canvas id="FilledLineChart"></canvas>
+        <canvas id="monthLineChart"></canvas>
+        <canvas id="annualLineChart" style={{ display: "none" }}></canvas>
       </div>
     </>
   );
