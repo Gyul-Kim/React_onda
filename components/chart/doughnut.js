79967b8fd338b499011e19ce28e7d960d550e5ee
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-06-01 17:48:27 +0900

    Update doughnut.js
    update doughnut.js (circle chart clear)

--- components/chart/doughnut.js
+++ components/chart/doughnut.js
@@ -1,17 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Chart } from "chart.js";
+import { decodeToken } from "../../provider/auth";
+import { GetCookie } from "../../provider/common";
 import style from "../../styles/Home.module.css";
+const axios = require("axios").default;
 
 function DoughnutChart() {
+  const [quotationData, setQuotationData] = useState(null);
+  const [replyData, setReplyData] = useState(null);
+  const [orderData, setOrderData] = useState(null);
+
   useEffect(() => {
+    const loadData = async () => {
+      const token = await GetCookie("ondaPcToken");
+      const tokenInfo = await decodeToken(token);
+      const orderInfoId = tokenInfo.payload.it_maker;
+
+      let dashboardURL =
+        process.env.ONDA_API_URL + "/api/dashboard/partner/" + orderInfoId;
+      const res = await axios.get(dashboardURL);
+      setQuotationData(res.data.data.totalCount);
+      setReplyData(res.data.data.totalCountReply);
+      setOrderData(res.data.data.totalCountOrder);
+    };
+
+    if (quotationData === null || replyData === null || orderData === null) {
+      loadData();
+    }
+
     var ctx = document.getElementById("myChart").getContext("2d");
     var myChart = new Chart(ctx, {
       type: "doughnut",
       data: {
-        labels: ["Hit", "Progress", "Miss"],
+        labels: ["견적요청", "견적회신", "주문건수"],
         datasets: [
           {
-            data: [70, 10, 6],
+            data: [quotationData, replyData, orderData],
             borderColor: [
               "rgb(75, 192, 192)",
               "rgb(255, 205, 86)",
@@ -46,10 +70,11 @@ function DoughnutChart() {
         },
       },
     });
-  }, []);
+  }, [quotationData, replyData, orderData]);
 
   return (
     <>
+      {/* Doughnut chart */}
       <div className={style.earning_chart}>
         <canvas id="myChart"></canvas>
       </div>
@@ -58,5 +83,3 @@ function DoughnutChart() {
 }
 
 export default DoughnutChart;
-
-
