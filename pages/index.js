8a9780247ca11e1e7a6212b9697509c283959895
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-06-02 16:58:32 +0900

    Update index.js
    connect from an index page to an introduce pages

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,25 @@ import { Link, Box } from "@chakra-ui/react";
 import IndexLayout from "../components/index_layout";
 import BoxIndicator from "../components/box/box";
 import ChartAll from "../components/chartAll";
-
+import GetCookie from "../provider/common";
+import { isMemberCheck, isGoingToIntro } from "../provider/auth";
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 import { GetReviewData } from "../pages/api/base";
 import style from "../styles/Home.module.css";
 
 export default function Home() {
   //페이지 로딩시 데이터 반영
-  useEffect(() => {}, []);
+  useEffect(() => {
+    isGoingToIntro();
+  }, []);
 
   return (
     <IndexLayout>
       <div className={style.main_container}>
         <BoxIndicator />
-       <ChartAll /> 
+        <ChartAll />
       </div>
     </IndexLayout>
   );
