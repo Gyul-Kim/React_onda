c90e725a37b12bebd5b4d7473df7d1e0187f8f04
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-06-07 17:42:03 +0900

    Update intro.js
    renew an intro page

--- pages/intro.js
+++ pages/intro.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import { Link, Box } from "@chakra-ui/react";
 import IndexLayout from "../components/no_header_layout";
 
@@ -5,6 +6,8 @@ import React, { useState, useEffect } from "react";
 import style from "../styles/Home.module.css";
 
 export default function Home() {
+  const handleAlert = () => alert("준비 중입니다.");
+
   useEffect(() => {}, []);
 
   return (
@@ -20,11 +23,12 @@ export default function Home() {
             <div className={style.main_center}>
               <h1>전자부품 온라인 소싱 솔루션</h1>
               <p>
-                온다파츠는 전자부품 마켓의 셀러에게 <br />
-                과잉, 잉여, 불용 재고를 판매하는데 <br />
-                필요한 솔루션을 제공하고, <br />
-                바이어들이 안전하게 <br />
-                거래할 수 있도록 돕습니다.
+                온다파츠는 전자부품 마켓의 셀러들이 <br />
+                간편하게 과잉, 잉여 재고를 판매할 수 있는 <br />
+                맞춤형 솔루션을 제공하고,
+                <br />
+                바이어들이 안심하고 <br />
+                거래할 수 있는 환경을 만들어 갑니다.
               </p>{" "}
               <br />
             </div>
@@ -38,7 +42,8 @@ export default function Home() {
               <div
                 className={style.cont_img}
                 style={{
-                  background: "url(./images/img_1.png) 0 0 no-repeat",
+                  background:
+                    "url(./video/ondaparts_introduce.gif) 0 0 no-repeat",
                   backgroundSize: "cover",
                 }}
               >
@@ -48,12 +53,13 @@ export default function Home() {
             <article className={style.cont_right}>
               <div className={style.cont_ex}>
                 <h3>
-                  매출 올리고 <br /> 더 많은 고객을 유치하는 방법 <br />
+                  매출 올리고
+                  <br /> 더 많은 고객을 만나는 방법, <br />
                   바로
                   <span className={style.title_bold}> 온다파츠</span>
                 </h3>
                 <p>
-                  온다파츠 판매자 맞춤형 대시보드에서
+                  판매자 전용 맞춤형 대시보드에서
                   <br />
                   견적하고 지금 바로 매출을 올려보세요.
                 </p>
@@ -68,12 +74,13 @@ export default function Home() {
             <article className={style.cont_left}>
               <div className={style.cont_ex}>
                 <h3>
-                  전자부품 재고 판매 <br /> 이제{" "}
-                  <span className={style.title_bold}>온다파츠</span>에서!
+                  전자부품 재고 판매,
+                  <br /> 이제 <span className={style.title_bold}>온다파츠</span>
+                  에서!
                 </h3>
                 <p>
-                  창고에 쌓여있는 전자부품의 과잉 재고로 <br /> 어려움을 겪고
-                  계신가요? <br />
+                  창고에 쌓여있는 과잉 재고로 <br /> 어려움을 겪고 계신가요?{" "}
+                  <br />
                 </p>
                 <p>
                   온다파츠는 웹 기반{" "}
@@ -83,17 +90,19 @@ export default function Home() {
                   을 <br /> 통해 글로벌 마켓으로 <br /> 판매할 수 있도록
                   지원합니다.
                 </p>
-                <a href="#">재고판매 신청하기</a>
+                <button href="#" onClick={handleAlert}>
+                  재고판매 신청하기
+                </button>
               </div>
             </article>
             <article className={style.cont_right}>
               <div
                 className={style.cont_img}
                 style={{
-                  background: "url(./images/img_3.png) 0 0 no-repeat",
+                  background: "url(./images/seller.gif) 0 0 no-repeat",
                   backgroundSize: "contain",
-                  width: "600px",
-                  height: "390px",
+                  width: "400px",
+                  height: "400px",
                 }}
               >
                 {/* <img src="" /> */}
@@ -107,10 +116,10 @@ export default function Home() {
               <div
                 className={style.cont_img}
                 style={{
-                  background: "url(./images/img_2.png) 0 0 no-repeat",
+                  background: "url(./images/buyer.gif) 0 0 no-repeat",
                   backgroundSize: "contain",
-                  width: "600px",
-                  height: "390px",
+                  width: "400px",
+                  height: "400px",
                 }}
               >
                 {/* <img src="" /> */}
@@ -119,14 +128,17 @@ export default function Home() {
             <article className={style.cont_right}>
               <div className={style.cont_ex}>
                 <h3>
-                  가장 완벽한 전자부품 견적 <br /> 쉽고 빠르고 안전하게
+                  가장 완벽한 전자부품 견적을
+                  <br /> 쉽고 빠르고 안전하게
                 </h3>
                 <p>
                   온다파츠에서 전 세계의 전자부품을 <br />{" "}
                   <span className={style.title_bold}>효율적</span>으로
                   소싱하세요.
                 </p>
-                <a href="#">온다파츠 견적 문의하기</a>
+                <button href="#" onClick={handleAlert}>
+                  견적 문의하기
+                </button>
               </div>
             </article>
           </section>
