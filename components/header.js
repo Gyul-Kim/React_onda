793e0964d9befe8cc42d9d6656b679618e82b6ec
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2023-06-08 17:40:08 +0900

    Update header.js
    clear header.js

--- components/header.js
+++ components/header.js
@@ -1,138 +1,88 @@
 import styles from "../styles/Home.module.css";
-import {
-  Box,
-  Button,
-  Center,
-  SimpleGrid,
-  Heading,
-  Flex,
-  Spacer,
-  Square,
-  Badge,
-} from "@chakra-ui/react";
+import { Box, Button, Center, Flex } from "@chakra-ui/react";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
-import { Typeahead, withAsync } from "react-bootstrap-typeahead";
-import { useRouter } from "next/router";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { GetCookie, GoSearch } from "../provider/common";
-import { authLogout } from "../provider/auth";
-import { useRecoilState } from "recoil";
-import { cartCntState, estimateCntState } from "../atoms/state";
-import { GetCartCnt } from "../pages/api/cart";
-import { getEstimateDataCnt } from "../pages/api/estimate";
-import Btn from "../components/common/btn";
-import { set } from "js-cookie";
 
-const AsyncTypeahead = withAsync(Typeahead);
+import { GetCookie } from "../provider/common";
+import { authLogout, decodeToken } from "../provider/auth";
 
-export default function Header(props) {
-  const router = useRouter();
-  const { keyword } = router.query; //미리 할당을 해줘야 처리됨
-  const [inputKeyword, setInputKeyword] = useState(keyword);
+export default function Header() {
   const [isLogin, setIsLogin] = useState(false);
-  const [isLoading, setisLoading] = useState(false);
-  const [options, setOptions] = useState([]);
-  const [cartBadge, setCartBadge] = useState("red");
-  const [cartCnt, setCartCnt] = useRecoilState(cartCntState);
-  const [estimateCnt, setEstimateCnt] = useRecoilState(estimateCntState);
-
-  useEffect(() => {
-    isLoginCheck();
-    showCnt();
-  }, []);
+  const [userName, setUserName] = useState();
 
   async function isLoginCheck() {
     const isLogin = await GetCookie("isLogin");
     setIsLogin(Number(isLogin));
-  }
 
-  const showCnt = async () => {
-    setCartCnt(await GetCartCnt());
-    //GetCartCnt();
-    const estimateCnt = await getEstimateDataCnt();
-    setEstimateCnt(estimateCnt);
-  };
+    if (isLogin == 1) {
+      const token = await GetCookie("ondaPcToken");
+      const tokenInfo = await decodeToken(token);
+      setUserName(tokenInfo.payload.name);
+    }
+  }
 
   // 로그아웃 기능
   const _handleLogout = async () => {
     await authLogout();
   };
 
-  const _handleAutocomplete = (query) => {
-    query = encodeURIComponent(query);
-    setisLoading(true);
-    setInputKeyword(query);
-    fetch(
-      process.env.SEARCH_API_URL + `/search?keyword=${query}&type=autocomplete`
-    )
-      .then((resp) => resp.json())
-      .then(({ data }) => {
-        const options = data.hits.hits.map((i) => ({
-          id: Number(i._id),
-          login: i._source.it_name,
-          it_name: i._source.it_name,
-        }));
-        setOptions(options);
-        setisLoading(false);
-      });
-  };
+  const handleAlert = () => alert("준비 중입니다.");
 
-  const onClick = async (e) => {
-    e.preventDefault();
-    if (!inputKeyword) {
-      alert("검색어를 입력하세요");
-    } else {
-      window.location.href =
-        "/search/lists?keyword=" + encodeURIComponent(inputKeyword);
-    }
-    return;
-  };
+  useEffect(() => {
+    isLoginCheck();
+  }, []);
 
-  const onChange = (e) => {
-    setInputKeyword(e.target.value);
-  };
   return (
     <>
       <Box className="header">
         <Box bg="" w="95%" pt={5} pb={5} color="#555" className="top-bar">
-          <Box
-            className="top-bar_box"
-            variant="ghost"
-            size="md"
-            fontSize={14}
-            p="0"
-          >
-            BETA
-          </Box>
           {isLogin === 1 ? (
-            <Box width="130px" className="flex-between">
-              <Box width="30px">
-                <Link href="/mypage/orders">mypage</Link>
-              </Box>
-
-              <Button
-                onClick={() => _handleLogout()}
-                className="logout-btn"
-                width="30px"
+            <>
+              <Box
+                className="top-bar_box"
+                variant="ghost"
+                size="md"
+                fontSize={14}
+                p="0"
               >
-                logout
-              </Button>
-            </Box>
-          ) : (
-            <Box width="280px" className="flex-between">
-              <Box width="30px" pl="10px">
-                <Link href="/auth/login">login</Link>
+                환영합니다! {userName} 님
               </Box>
-            </Box>
+              <Box className="flex-between">
+                <Button
+                  onClick={() => _handleLogout()}
+                  className="logout-btn"
+                  width="30px"
+                >
+                  logout
+                </Button>
+              </Box>
+            </>
+          ) : (
+            <>
+              <Box
+                className="top-bar_box"
+                variant="ghost"
+                size="md"
+                fontSize={14}
+                p="0"
+              >
+                환영합니다!
+              </Box>
+              <Box width="fit-content" className="flex-between">
+                <Box pl="10px">
+                  <Link href="/auth/login" passHref>
+                    login
+                  </Link>
+                </Box>
+              </Box>
+            </>
           )}
         </Box>
         <Box className="search-bar">
           <Flex color="white" w="95%" h="80px" className="search-gnb">
             <Center w="260px!important">
               <Box className={"header-logo cursor"} w>
-                <Link href="/" className="onda-logo">
+                <Link href="/" className="onda-logo" passHref>
                   <img
                     src="/images/ondalogo.png"
                     alt="로고"
@@ -143,14 +93,19 @@ export default function Header(props) {
             </Center>
 
             <Box className={"header-link"}>
-              <Link p="4" href="quotation/quotation_reply_list">
+              <Link p="4" href="/quotation/quotation_reply_list" passHref>
                 판매관리
               </Link>
             </Box>
             <Box className={"header-link"}>
-              <Link p="4" href="#" onClick={() => {}} className={"header-link"}>
+              <div
+                p="0"
+                onClick={handleAlert}
+                className={"header-link"}
+                style={{ marginLeft: "0", cursor: "pointer" }}
+              >
                 재고관리
-              </Link>
+              </div>
             </Box>
           </Flex>
         </Box>
@@ -158,3 +113,4 @@ export default function Header(props) {
     </>
   );
 }
+
